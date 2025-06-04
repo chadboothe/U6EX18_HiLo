9480710bb41fb0a25e8dd12f0d330c4d9e624f4d
chadboothe <43538127+chadboothe@users.noreply.github.com>
2025-06-04 10:29:00 -0400

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,19 +8,6 @@ class MainTest {
 
 
     @Test
-    void countWords_One() {
-        assertEquals(11, Main.countWords("There was no telling what thoughts would come from the machine."));
 
-    }
-
-    @Test
-    void countWords_Two() {
-        assertEquals(6, Main.countWords("Joyce enjoyed eating pancakes with ketchup."));
-    }
-
-    @Test
-    void countWords_Three() {
-        assertEquals(13, Main.countWords("He watched the dancing piglets with panda bear tummies in the swimming pool."));
-    }
 
 }
